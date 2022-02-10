ebb6a81630e9840748503c9bc0eb39c556ac895c
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2022-02-10 08:43:43 +0530

    Add files via upload

---
+++ GUIFTPClient/src/fdsfsdfdsf/JFilePicker.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fdsfsdfdsf;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class JFilePicker extends JPanel {
+	private String textFieldLabel;
+	private String buttonLabel;
+	
+	private JLabel label;
+	private JTextField textField;
+	private JButton button;
+	
+	private JFileChooser fileChooser;
+	
+	private int mode;
+	public static final int MODE_OPEN = 1;
+	public static final int MODE_SAVE = 2;
+	
+	public JFilePicker(String textFieldLabel, String buttonLabel) {
+		this.textFieldLabel = textFieldLabel;
+		this.buttonLabel = buttonLabel;
+		
+		fileChooser = new JFileChooser();
+		
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		// creates the GUI
+		label = new JLabel(textFieldLabel);
+		
+		textField = new JTextField(30);
+		button = new JButton(buttonLabel);
+		
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				buttonActionPerformed(evt);				
+			}
+		});
+		
+		add(label);
+		add(textField);
+		add(button);
+		
+	}
+	
+	private void buttonActionPerformed(ActionEvent evt) {
+		if (mode == MODE_OPEN) {
+			if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+				textField.setText(fileChooser.getSelectedFile().getAbsolutePath());
+			}
+		} else if (mode == MODE_SAVE) {
+			if (fileChooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+				textField.setText(fileChooser.getSelectedFile().getAbsolutePath());
+			}
+		}
+	}
+
+		
+	public void setMode(int mode) {
+		this.mode = mode;
+	}
+	
+	public String getSelectedFilePath() {
+		return textField.getText();
+	}
+	
+	public JFileChooser getFileChooser() {
+		return this.fileChooser;
+	}
+}
